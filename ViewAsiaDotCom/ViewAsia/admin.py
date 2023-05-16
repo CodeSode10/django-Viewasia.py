6202ba2b767d24d6562b34b246239fd07d986fa5
Code With Nc <123615985+CodeWithNc@users.noreply.github.com>
2023-05-16 17:09:16 +0530

    Delete ViewAsiaDotCom directory

--- ViewAsiaDotCom/ViewAsia/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(ViewImages)
-admin.site.register(ImageCategory)
-admin.site.register(ContactModel)
-admin.site.register(UploadModel)
