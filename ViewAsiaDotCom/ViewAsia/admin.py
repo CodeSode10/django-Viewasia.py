6cc943e9aa0a69a7d2580049608d9c1a779b7aa9
Code With Nc <123615985+CodeWithNc@users.noreply.github.com>
2023-05-16 16:48:52 +0530

    Initial commit

---
+++ ViewAsiaDotCom/ViewAsia/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(ViewImages)
+admin.site.register(ImageCategory)
+admin.site.register(ContactModel)
+admin.site.register(UploadModel)
