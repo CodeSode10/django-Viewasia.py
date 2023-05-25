ed976509af9a5fde9c278d0322b6aaef53b30d52
Code With Hindi <123615985+CodeWithHindi@users.noreply.github.com>
2023-05-26 01:46:17 +0530

    Update settings.py

--- ViewAsiaDotCom/ViewAsiaDotCom/settings.py
+++ ViewAsiaDotCom/ViewAsiaDotCom/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k(j3hbj#gb_5+xi@6^u#(f4r2a9%81vgo756$mzb9k*d^d8fhf'
+SECRET_KEY = 'create_your_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
